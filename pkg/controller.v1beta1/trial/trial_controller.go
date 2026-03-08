422e6782ff27517cd1283c13034c816250e86244
abhijeet-dhumal <abhijeetdhumal652@gmail.com>
2026-03-09 01:09:20 +0530

    Fix: Early stopping now works during training for TrainJob
    - Fixed GetDeployedJobStatus to return JobRunning when trial is already
      running but hasn't succeeded/failed yet. Previously it returned nil
      which caused the early stopping check to be skipped.
    
    - Removed debug logging added during investigation.
    
    This enables MedianStop early stopping to prune poorly performing trials
    during training when using TrainerStatusCollector with TrainJob.

--- pkg/controller.v1beta1/trial/trial_controller.go
+++ pkg/controller.v1beta1/trial/trial_controller.go
@@ -286,26 +286,12 @@ func (r *ReconcileTrial) reconcileTrial(instance *trialsv1beta1.Trial) error {
 			}
 
 			// If job status not available yet, requeue to poll for updates
-			logger.Info("Job status check",
-				"jobStatusNil", jobStatus == nil,
-				"hasMetrics", observation != nil)
 			if jobStatus == nil {
 				return errTrainerStatusPolling
 			}
 
 			// Evaluate early stopping rules during training
 			// For TrainerStatusCollector, we handle this in the controller since there's no sidecar
-			obsMetricsCount := 0
-			if observation != nil {
-				obsMetricsCount = len(observation.Metrics)
-			}
-			logger.Info("Checking early stopping condition",
-				"jobStatusNil", jobStatus == nil,
-				"jobCondition", func() string { if jobStatus != nil { return string(jobStatus.Condition) }; return "nil" }(),
-				"observationNil", observation == nil,
-				"observationMetrics", obsMetricsCount,
-				"rulesCount", len(instance.Spec.EarlyStoppingRules),
-				"progressPct", func() int32 { if trainingProgress != nil { return trainingProgress.ProgressPercentage }; return -1 }())
 			if jobStatus != nil && jobStatus.Condition == trialutil.JobRunning && observation != nil {
 				if evaluateEarlyStoppingRules(instance, observation, trainingProgress) {
 					logger.Info("Early stopping rule triggered, stopping trial")
--- pkg/controller.v1beta1/trial/trial_controller_util.go
+++ pkg/controller.v1beta1/trial/trial_controller_util.go
@@ -247,13 +247,7 @@ func getTrainingProgressFromTrainJob(deployedJob *unstructured.Unstructured, exi
 
 	// Extract metrics from trainerStatus.metrics
 	var observation *commonv1beta1.Observation
-	metricsRaw, metricsOk := status["metrics"].([]interface{})
-	log.Info("getTrainingProgressFromTrainJob debug",
-		"job", deployedJob.GetName(),
-		"hasMetrics", metricsOk,
-		"metricsLen", len(metricsRaw),
-		"progressPct", progress.ProgressPercentage)
-	if metricsOk && len(metricsRaw) > 0 {
+	if metricsRaw, ok := status["metrics"].([]interface{}); ok && len(metricsRaw) > 0 {
 		observation = &commonv1beta1.Observation{
 			Metrics: []commonv1beta1.Metric{},
 		}
